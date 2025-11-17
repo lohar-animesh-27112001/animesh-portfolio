bb048fedea15ade740c9ca5afbac5257cea76c0e
Animesh Lohar <83830039+lohar-animesh-27112001@users.noreply.github.com>
2025-11-17 18:31:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -151,17 +151,11 @@ document.getElementById("contactForm").addEventListener("submit", async (e) => {
 
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector("nav ul");
+const navLinks = document.querySelectorAll("nav ul li");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
-});
-
-navLinks.forEach(link => {
-    link.addEventListener("click", () => {
-        hamburger.classList.remove("active");
-        navMenu.classList.remove("active");
-    });
 });
 
 navLinks.forEach(link => {
